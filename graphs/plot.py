aaa9182b4297955a248bbd65a2e77a351ffc471b
Ekaterina Sakharova <kates@ebi.ac.uk>
2020-02-17 16:01:23 +0000

    readme

--- graphs/plot.py
+++ graphs/plot.py
@@ -1,4 +1,7 @@
 import subprocess
+import argparse
+import sys
+
 
 def plot_graph(name):
 
@@ -7,7 +10,15 @@ def plot_graph(name):
     bashCommand = "neato -T png dots/{n}.dot > png/{n}.png".format(n=name)
     subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
 
-list_pathways = '/Users/kates/Desktop/EBI/CWL/kegg-cwl/pathways/list_pathways.txt'
-with open(list_pathways, 'r') as file_pathways:
-    for line in file_pathways:
-        plot_graph(line.strip())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generates Graphs for each contig")
+    parser.add_argument("-l", "--pathways", dest="list_pathways", help="all_pathways.txt", required=True)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        with open(args.list_pathways, 'r') as file_pathways:
+            for line in file_pathways:
+                plot_graph(line.strip())
