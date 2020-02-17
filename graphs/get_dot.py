abaf0e34a35fc1e0c47acb06357186cb42dfd7a5
Ekaterina Sakharova <kates@ebi.ac.uk>
2020-02-17 15:47:41 +0000

    new pics

--- graphs/get_dot.py
+++ graphs/get_dot.py
@@ -1,10 +1,14 @@
+import os
+import argparse
+import sys
 import pickle
 import networkx as nx
-import os
 
-def create_dot(graph, name):
+
+def create_dot(graph, name, pathway):
     with open(os.path.join('dots', name + '.dot'), 'w') as dot_file:
         dot_file.write("digraph G {\n"
+                       "graph [label=\"" + name + "\n" + pathway + "\",fontsize=20];\n"
                        "node [shape=box,style=filled];\n"
                        "edge [len=3,color=grey];\n"
                        "{node [width=.3,height=.3,shape=octagon,style=filled,color=skyblue] ")
@@ -18,12 +22,27 @@ def create_dot(graph, name):
             number = edge[2]
             label = edges._adjdict[from_node][to_node][number]['label']
             weight = edges._adjdict[from_node][to_node][number]['weight']
-            dot_file.write(str(from_node) + ' -> ' + str(to_node) + ' [label="' + label + ' [' + str(weight) + ']"];\n')
+            if weight == 1 or weight == 0 :
+                weight_str = str(weight)
+            else:
+                weight_str = '1/' + str(int(1/weight))
+            dot_file.write(str(from_node) + ' -> ' + str(to_node) + ' [label="' + label + ' [' + weight_str + ']"];\n')
         dot_file.write('}')
 
 
-path_to_graphs = "graphs.pkl"
-graph_file = open(path_to_graphs, 'rb')
-graphs = pickle.load(graph_file)
-for name in graphs:
-    create_dot(graphs[name], name)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generates Graphs for each contig")
+    parser.add_argument("-g", "--input", dest="input_file", help="graphs.pkl", required=True)
+    parser.add_argument("-l", "--pathways", dest="list_pathways", help="all_pathways.txt", required=True)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        graph_file = open(args.input_file, 'rb')
+        graphs = pickle.load(graph_file)
+        with open(args.list_pathways, 'r') as list_pathways:
+            for line in list_pathways:
+                line = line.strip()
+                name, pathway = line.split(':')
+                create_dot(graphs[name], name, pathway)
